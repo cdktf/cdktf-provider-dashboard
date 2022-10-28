9696fd54585c397eaa9ea3f9f05bc98bb1798399
Ansgar Mertens <ansgar@hashicorp.com>
2022-10-28 10:40:03 +0200

    feat: show failing repos first

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,34 @@
+function repoHasFailingWorkflow(repo) {
+  return Object.values(repo.workflows).some(
+    (workflowItems) => workflowItems[0].conclusion != "success"
+  );
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("style.css");
+
+  // sorts repositories with failing CI to the front
+  eleventyConfig.addNunjucksFilter("sortRepos", function (value) {
+    if (!Array.isArray(value))
+      throw new Error("Expected an array for sortRepos filter");
+
+    return value.sort(function (a, b) {
+      if (repoHasFailingWorkflow(a)) {
+        return -1;
+      } else if (repoHasFailingWorkflow(b)) {
+        return 1;
+      }
+      return 0;
+    });
+  });
+
   return {
     dir: {
       input: "src",
       data: "_data",
       includes: "_includes",
-      layouts: "_layouts"
+      layouts: "_layouts",
     },
-    pathPrefix: "/check-prebuilt-provider-status/"
+    pathPrefix: "/check-prebuilt-provider-status/",
   };
-}
+};
