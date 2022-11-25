bcd8b216bfab5885b7f41d2008fcb32bf9772a08
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2022-11-25 16:23:26 +0100

    chore: Add workflow age

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,10 @@
-function repoHasFailingWorkflow(repo) {
-  return Object.values(repo.workflows).some(
-    (workflowItems) => workflowItems[0].conclusion != "success"
-  );
-}
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("style.css");
 
-  // sorts repositories with failing CI to the front
-  eleventyConfig.addNunjucksFilter("sortRepos", function (value) {
-    if (!Array.isArray(value))
-      throw new Error("Expected an array for sortRepos filter");
-
-    return value.sort(function (a, b) {
-      if (repoHasFailingWorkflow(a)) {
-        return -1;
-      } else if (repoHasFailingWorkflow(b)) {
-        return 1;
-      }
-      return 0;
-    });
-  });
+  // Filters
+  eleventyConfig.addFilter("sortRepos", require('./src/filters/sortRepos'))
+  eleventyConfig.addFilter("daysAgo", require('./src/filters/daysAgo'))
 
   return {
     dir: {
