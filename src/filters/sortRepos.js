e0c1d39392d6663393772ec32eb1ecc13f3a3c27
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2022-11-28 12:28:56 +0100

    chore: sort with out of date problems

--- src/filters/sortRepos.js
+++ src/filters/sortRepos.js
@@ -1,22 +1,22 @@
+const semver = require("semver")
+
 function repoCountFailingWorkflows(repo) {
     return Object.values(repo.workflows).reduce(
         (res, workflowItems) => res += workflowItems[0].conclusion === "success" ? 0 : 1
         , 0);
 }
 
-function repoHasPullRequests(repo) {
-    return repo.pulls.length > 0
-}
-
-function repoHasIssues(repo) {
-    return repo.issues.length > 0
+function isMajorOutofDate(repo) {
+    if (!repo.provider) return 0;
+    return semver.diff(repo.packageJson.cdktf.provider.version, repo.provider.latestVersion) === "major" ? 1 : 0
 }
 
 function sortByProblems(a, b) {
     return repoCountFailingWorkflows(b) - repoCountFailingWorkflows(a) ||
-        b.pulls.length - a.pulls.length ||
+        isMajorOutofDate(b) - isMajorOutofDate(a) ||
+        a.latestRelease.published_at.localeCompare(b.latestRelease.published_at) ||
         b.issues.length - a.issues.length ||
-        b.latestRelease.published_at.localeCompare(a.latestRelease.published_at)
+        b.pulls.length - a.pulls.length
 }
 
 
